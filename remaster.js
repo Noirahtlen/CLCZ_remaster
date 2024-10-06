3743996043583637b5cd1ab214ef35c3c1b863ba
Noirahtlen <acc10049@gmail.com>
2024-10-06 14:08:23 +0800

    Update remaster.js

--- remaster.js
+++ remaster.js
@@ -11,7 +11,7 @@ var date_1 = '-01-01';
 var date_2 = '-12-31';
 
 //PART1 cloud remove and 3-year
-
+//mask functions
 function maskL8sr(image) {
   // Bit 0 - Fill
   // Bit 1 - Dilated Cloud
@@ -52,7 +52,7 @@ function maskL457sr(image) {
       .updateMask(saturationMask);
 }
 
-
+//image sources
 var l8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
                   .filter(ee.Filter.calendarRange(1,12,'month'))
                   .filterBounds(bound);
@@ -64,13 +64,14 @@ var l7 = ee.ImageCollection('LANDSAT/LE07/C02/T1_L2')
 var l5 = ee.ImageCollection('LANDSAT/LT05/C02/T1_L2')
                   .filter(ee.Filter.calendarRange(1,12,'month'))
                   .filterBounds(bound);
-                  
+
+//bands 
 var bands8 = ['SR_B2','SR_B3','SR_B4','SR_B5','SR_B6','SR_B7'];
 var bands75 = ['SR_B1','SR_B2','SR_B3','SR_B4','SR_B5','SR_B7'];
 
 //change function, bands, images with year
 var yourfunction = ee.Algorithms.If(
-  year <= 2014,    //if earlier than 2014, use L7;
+  year <= 2014,    //if <= 2014, use L7;
   maskL457sr, 
   ee.Algorithms.If(
     year <= 2020,  //if 2015-2020, use L8;
@@ -79,7 +80,7 @@ var yourfunction = ee.Algorithms.If(
 );
 
 var yourimage = ee.Algorithms.If(
-  year <= 2014,    //if earlier than 2014, use L7;
+  year <= 2014,    //if <= 2014, use L7;
   l7,             //
   ee.Algorithms.If(
     year <= 2020,  //if 2015-2020, use L8;
@@ -88,7 +89,7 @@ var yourimage = ee.Algorithms.If(
 );
 
 var yourband = ee.Algorithms.If(
-  year <= 2014,    //if earlier than 2014, use L7;
+  year <= 2014,    //if <= 2014, use L7;
   bands8,             //
   ee.Algorithms.If(
     year <= 2020,  //if 2015-2020, use L8;
@@ -96,21 +97,44 @@ var yourband = ee.Algorithms.If(
   )
 );
 
-//Map.addLayer(bound)
 // Map the function over one year of data. for 2015-2020 L8, for 2000-2014 L7
-/*var collection = l8
+var collection = yourimage
                      .filterDate(year+'-01-01', year+'-12-31')
                      .map(yourfunction);
 
+//for every year, we have cloud-free 'composite' image of L7/L8
 var composite = collection.median();
-var composite = composite.select(bands8).clip(bound);*/
+var composite = composite.select(yourband).clip(bound);
 
-var collection = l7
-                     .filterDate(year+'-01-01', year+'-12-31')
-                     .map(maskL457sr);
+//PART2 PCA calculation
+//Replace with the image from the cloud removal and three-year image compositing step
+var arrayImage = composite.select(yourband).toArray();
 
-//for every year, we have clouc-free 'composite' image of L7/L8
-var composite = collection.median();
-var composite = composite.select(bands75).clip(bound);
+var covar = arrayImage.reduceRegion({
+  reducer: ee.Reducer.covariance(),
+  scale:30,
+  geometry:bound,
+  maxPixels:1e13
+})
 
-//Next, PART2 PCA calculation
+var covarArray = ee.Array(covar.get('array'))
+
+var eigens = covarArray.eigen()
+var eigenVectors = eigens.slice(1,1)
+
+var principalComponents = ee.Image(eigenVectors).matrixMultiply(arrayImage.toArray(1));
+
+var pcImage = principalComponents
+// Throw out an unneeded dimension, [[]] -> [].
+.arrayProject([0])
+// Make the one band array image a multi-band image, [] -> image
+.arrayFlatten([['pc1','pc2','pc3','pc4','pc5','pc6']])
+
+//we have PC1 as pca result for every year
+var pc1 = pcImage.select('pc1')
+var pc1 = pc1.multiply(1e8).toInt()
+var pc1 = pc1.clip(bound)
+
+//PART3 texture feature extraction
+
+
